f17e55cb86a3e34c67b94b89ef027f4065bd783c
AyushKumarThakur21BCE3771 <144116323+AyushKumarThakur21BCE3771@users.noreply.github.com>
2024-03-25 02:52:57 +0530

    first commit

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect("mongodb+srv://ayush524425:cSPRROO61KpLe8Wb@cluster0.ovbanc7.mongodb.net/chatie-app-db?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
@@ -10,3 +10,5 @@ const connectToMongoDB = async () => {
 };
 
 export default connectToMongoDB;
+
+// cSPRROO61KpLe8Wb
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -9,7 +9,7 @@ const protectRoute = async (req, res, next) => {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
 		}
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, "xdNkSnjiq8VhFXSwEqBMMNQIhMFiuiIUoDqE7CnH+RU=");
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
